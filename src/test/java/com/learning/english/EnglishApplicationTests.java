9ac566a422f2530b14565c1edc9c6db4eb8181a1
Pawel Kozera <pawelkozera1@gmail.com>
2024-07-30 17:12:27 +0200

    init

---
+++ src/test/java/com/learning/english/EnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
