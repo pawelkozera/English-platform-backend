d33c93d2547bf329caa1da0f1a981966727b9e94
Pawel Kozera <pawelkozera1@gmail.com>
2024-12-22 14:55:36 +0100

    Added tests for Base64Util

---
+++ src/test/java/com/learning/english/Base64UtilTest.java
@@ -0,0 +1,61 @@
+package com.learning.english;
+
+import com.learning.english.utils.Base64Util;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Base64UtilTest {
+
+    @Test
+    void shouldEncodeDataToBase64() {
+        String originalData = "Hello World";
+
+        String encodedData = Base64Util.encode(originalData);
+
+        assertNotNull(encodedData);
+        assertEquals("SGVsbG8gV29ybGQ=", encodedData);
+    }
+
+    @Test
+    void shouldDecodeBase64ToOriginalData() {
+        String encodedData = "SGVsbG8gV29ybGQ=";
+
+        String decodedData = Base64Util.decode(encodedData);
+
+        assertNotNull(decodedData);
+        assertEquals("Hello World", decodedData);
+    }
+
+    @Test
+    void shouldEncodeAndDecodeRoundtrip() {
+        String originalData = "Test String";
+
+        String encodedData = Base64Util.encode(originalData);
+        String decodedData = Base64Util.decode(encodedData);
+
+        assertEquals(originalData, decodedData);
+    }
+
+    @Test
+    void shouldHandleEmptyString() {
+        String emptyString = "";
+
+        String encodedData = Base64Util.encode(emptyString);
+        String decodedData = Base64Util.decode(encodedData);
+
+        assertEquals(emptyString, decodedData);
+    }
+
+    @Test
+    void shouldHandleNullData() {
+        String nullString = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            Base64Util.encode(nullString);
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            Base64Util.decode(nullString);
+        });
+    }
+}
